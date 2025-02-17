40d92b9e14feb02a1aae9600e8c7a88eaad7bd95
Kinsagi Yuirin <sengsoon02@gmail.com>
2025-02-17 16:32:28 +0700

    Fix

--- Assets/DungeonTogether/Scripts/Player/PlayerMovement.cs
+++ Assets/DungeonTogether/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMovement : NetworkBehaviour
 {
@@ -8,7 +9,8 @@ public class PlayerMovement : NetworkBehaviour
     [Header("References")]
     [SerializeField] private InputReader inputReader;
     [SerializeField] private Transform playerTransform;
-    [SerializeField] private Rigidbody2D rigidbody2D;
+    [SerializeField] private Rigidbody2D rb2d;
+    [SerializeField] private SpriteRenderer spriteRenderer;
     
     [Header("Settings")]
     [SerializeField] private float movementSpeed = 4f;
@@ -23,16 +25,16 @@ public class PlayerMovement : NetworkBehaviour
             return;
         }
         movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        movementInput.Normalize();    
+        movementInput.Normalize();
+        
+        Debug.Log($"Movement Input: {movementInput}");
     }
     
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        if (!IsOwner)
-        {
-            return;
-        } 
-        rigidbody2D.velocity = movementInput * movementSpeed;
+        if (!IsOwner) return;
+        
+        rb2d.linearVelocity = movementInput * movementSpeed;
     }
     
     private void HandleMove(Vector2 movement)
